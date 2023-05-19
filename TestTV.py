b44d4c4f5bbe7dff840f43dbc0ee1482a123a207
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-05-19 22:25:52 +0800

    added pseudocode

--- TestTV.py
+++ TestTV.py
@@ -4,7 +4,21 @@
 from ClassTV import TV
 
 # call TV1 and print channel and volume
-tv1 = TV(0, 0, False) # add object commit
+tv1 = TV(0, 0, False)
+
+# set channel
+# channel up
+# set volume
+# volume up
+# turn on
+
 
 # call TV2 and print channel and volume
-tv2 = TV(0, 0, False) #  add object commit
+tv2 = TV(0, 0, False)
+
+# set channel
+# channel down
+# set volume
+# volume down
+# turn on
+
