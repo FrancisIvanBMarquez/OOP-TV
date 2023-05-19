1b42df4b1c0bce1d398a6f50210cc54b301727ea
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-05-19 21:19:13 +0800

    Pseudocode

--- ClassTV.py
+++ ClassTV.py
@@ -1,1 +1,5 @@
 # Marquez, Francis Ivan B.,_BSCpE 1-5
+
+# class
+# constructor
+# behaviors
---
+++ TestTV.py
@@ -0,0 +1,5 @@
+# Marquez, Francis Ivan B.,_BSCpE 1-5
+
+# import ClassTV
+# call TV1 and print channel and volume
+# call TV2 and print channel and volume
