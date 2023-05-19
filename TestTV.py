7686a63fcd025ff676f8648fda7562526c0c03ca
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-05-19 22:16:37 +0800

    import TV added, might not be working

--- TestTV.py
+++ TestTV.py
@@ -1,5 +1,10 @@
 # Marquez, Francis Ivan B.,_BSCpE 1-5
 
 # import ClassTV
+from ClassTV import TV
+
 # call TV1 and print channel and volume
+tv1 = TV(0, 0, False)
+
 # call TV2 and print channel and volume
+tv2 = TV(0, 0, False)
