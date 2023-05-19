f5c4542cf59bd8dd2fa2a214c97b50a2bcba5261
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-05-19 22:21:09 +0800

    added objects tv1 and tv2
    forgot to commit the import only, and the object addition  also got added to previous commit. Only highlighting commit with comment.

--- TestTV.py
+++ TestTV.py
@@ -4,7 +4,7 @@
 from ClassTV import TV
 
 # call TV1 and print channel and volume
-tv1 = TV(0, 0, False)
+tv1 = TV(0, 0, False) # add object commit
 
 # call TV2 and print channel and volume
-tv2 = TV(0, 0, False)
+tv2 = TV(0, 0, False) #  add object commit
