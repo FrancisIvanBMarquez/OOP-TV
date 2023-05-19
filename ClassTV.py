5473d9069b195a4303899765b25f22093fd1842f
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-05-19 21:36:06 +0800

    GetChannel added

--- ClassTV.py
+++ ClassTV.py
@@ -3,8 +3,13 @@
 # class
 class TV:
     # constructor
-    def __init__(self, channel, volume_level, power):
+    def __init__(self, channel, volume_level, power_on):
         self.channel = channel
         self.volume_level = volume_level
-        self.power = power
+        self.power_on = power_on
     # behaviors
+    # channel
+    def GetChannel(self):
+        print(self.channel)
+    # volume
+    # power
